e644b738acb8f38516a48a005862319548a9552c
Nick_S1 <carlos.qs.01.cqs@gmail.com>
2020-02-03 11:10:15 +0100

    versión comentada

--- geometria/src/Triangulo_CQS.java
+++ geometria/src/Triangulo_CQS.java
@@ -1,8 +1,22 @@
 
+/**
+ * Clase Triangulo
+ * @author Carlos Quiles Serrano
+ * @version 1.2
+ */
+
 public class Triangulo_CQS extends FiguraGeometrica_CQS {
 	private double lado1;
 	private double lado2;
 	private double lado3;
+	
+	/**
+	 * Constructor Triangulo
+	 * @param tipoFigura Nombre
+	 * @param lado1 Lado 1
+	 * @param lado2 Lado 2
+	 * @param lado3 Lado 3
+	 */
 	
 	public Triangulo_CQS(String tipoFigura, double lado1, double lado2, double lado3) {
 		super(tipoFigura);
@@ -11,11 +25,19 @@ public class Triangulo_CQS extends FiguraGeometrica_CQS {
 		this.lado3 = lado3;
 	}
 
+	/**
+	 * Cálculo de Perímetro
+	 */
+	
 	@Override
 	public double perimetro() {
 		return lado1 + lado2 + lado3;
 	}
 
+	/**
+	 * Cálculo de Area
+	 */
+	
 	@Override
 	public double area() { //Utiliza la formula de Her—n
 		double sp;
